7a5254b2689fdbaa7e8c946a60d2996478ff672f
Yanzhen Yu <yanzhen@smartx.com>
2021-03-02 13:07:20 +0800

    export mirror2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import record from './record';
 import { Replayer } from './replay';
-import { mirror } from './utils';
+import { mirror, mirror2 } from './utils';
 import * as utils from './utils';
 
 export {
@@ -13,4 +13,4 @@ export {
 const { addCustomEvent } = record;
 const { freezePage } = record;
 
-export { record, addCustomEvent, freezePage, Replayer, mirror, utils };
+export { record, addCustomEvent, freezePage, Replayer, mirror, mirror2, utils };
